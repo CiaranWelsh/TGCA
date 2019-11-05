c37b77a5f05a1210db25df0fc76a896403d4fc46
CiaranWelsh <ciaran.welsh@newcastle.ac.uk>
2019-11-05 14:36:25 +0000

    update slurm script

--- feature_selection_slurm.sh
+++ feature_selection_slurm.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=feature_selection01
-#SBATCH --output=cv_analysis_eis-%j-%a.out
-#SBATCH --error=cv_analysis_eis-%j-%a.err
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
 
-
-#!/bin/bash
-FLAGS="--ntasks=1 --cpus-per-task=1"
 for num_features in {1..20}; do
     for num_clusters in {3..20}; do
-        sbatch python ga_for_cluster.py $num_features $num_clusters
+        FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_${num_features}_${num_clusters}"
+        sbatch $FLAGS ga_for_cluster.py $num_features $num_clusters
     done
 done
