693b3a3a4c0ce9fd6725e53823f28959879dcf35
CiaranWelsh <ciaran.welsh@newcastle.ac.uk>
2019-11-05 14:43:41 +0000

    update slurm scirpt

--- feature_selection_slurm.sh
+++ feature_selection_slurm.sh
@@ -2,8 +2,7 @@
 
 for num_features in {1..20}; do
   for num_clusters in {3..20}; do
-    echo $num_clusters
-    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_$num_features_$num_clusters"
+    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_{$num_features}_$num_clusters"
     sbatch $FLAGS --wrap="tgca/ga_for_cluster.py $num_features $num_clusters"
   done
 done
