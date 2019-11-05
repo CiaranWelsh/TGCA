321691c1d50ecd636286771e1855871b143f2a4d
CiaranWelsh <ciaran.welsh@newcastle.ac.uk>
2019-11-05 14:47:35 +0000

    update slurm scirpt

--- feature_selection_slurm.sh
+++ feature_selection_slurm.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+conda activate py36
 
-for num_features in {1..20}; do
-  for num_clusters in {3..20}; do
-    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_{$num_features}_$num_clusters"
+for num_features in {1..10}; do
+  for num_clusters in {3..10}; do
+    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_{$num_features}_{$num_clusters}"
     sbatch $FLAGS --wrap="tgca/ga_for_cluster.py $num_features $num_clusters"
   done
 done
