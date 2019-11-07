d7e321249ef0a1f8aaab09a2ce0c198d2156548c
Ciaran Welsh <ciaran.welsh@newcastle.ac.uk>
2019-11-07 16:21:07 +0000

    change parameters

--- feature_selection_slurm.sh
+++ feature_selection_slurm.sh
@@ -4,8 +4,8 @@ conda activate py36
 wd="/mnt/nfs/home/b7053098/ciaran/TGCA"
 echo $wd
 echo ${wd}/tgca/ga_for_cluster.py
-for num_features in {1..20}; do
-  for num_clusters in {3..25}; do
+for num_features in {1..15}; do
+  for num_clusters in {2..6}; do
     echo "${num_features}_${num_clusters}"
     FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=f_${num_features}_k_${num_clusters}"
     sbatch $FLAGS --wrap="python ${wd}/tgca/ga_for_cluster.py runner $num_clusters $num_features"
