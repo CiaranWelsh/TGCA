f7656e21382e907c91a30257c0369d4fb54dba47
Krutik Patel (PGR) <b7053098@login01.cluster>
2019-11-07 14:17:04 +0000

    update feature sel script

--- feature_selection_slurm.sh
+++ feature_selection_slurm.sh
@@ -4,10 +4,10 @@ conda activate py36
 wd="/mnt/nfs/home/b7053098/ciaran/TGCA"
 echo $wd
 echo ${wd}/tgca/ga_for_cluster.py
-for num_features in {1..50}; do
+for num_features in {1..20}; do
   for num_clusters in {3..25}; do
     echo "${num_features}_${num_clusters}"
-    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=fs_${num_features}_${num_clusters}"
-    sbatch $FLAGS --wrap="python ${wd}/tgca/ga_for_cluster.py $num_features $num_clusters"
+    FLAGS="--ntasks=1 --cpus-per-task=1 --job-name=f_${num_features}_k_${num_clusters}"
+    sbatch $FLAGS --wrap="python ${wd}/tgca/ga_for_cluster.py runner $num_clusters $num_features"
   done
 done
